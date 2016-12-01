b8b5d11499c6f5f1cfa450d5427320fe789c56c7
Marcos Hosoya <marcos.hosoya@certsys.com.br>
2016-12-01 13:43:24 -0200

    First commit, with an app made with ReactJS

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,31 @@
 var path = require('path');
+
+var APP_DIR = path.resolve(__dirname, 'app')
+var BUILD_DIR = path.resolve(__dirname, 'build');
+
+
 var config = {
-  entry: path.resolve(__dirname, 'app/main.jsx'),
+  entry: APP_DIR + '/main.jsx',
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devtool: 'source-map',
+  devServer: {
+    inline: true,
+    contentBase: BUILD_DIR,
+    port: 3333
   },
   module: {
     loaders: [{
       test: /\.jsx?$/,
-      loader: 'babel'
+      include: APP_DIR,
+      filename: 'bundle.js',
+      loader: 'babel',
+      query: {
+	presets: ['es2015','react']
+	}
     }]
   }
 };
