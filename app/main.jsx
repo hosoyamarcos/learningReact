3873abc47dbcd091345023a3655ab689ba223dbf
Marcos Teruaki Hosoya <marcos.hosoya@certsys.local>
2016-12-05 15:38:43 -0200

    add components

--- app/main.jsx
+++ app/main.jsx
@@ -1,108 +1,49 @@
-var React = require('react');
+import React from 'react'
 import ReactDOM from 'react-dom'
 
+class AnotherComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>another component</p>
+            </div>
+        )
+    }
+}
 
-var X = React.createClass({
-  render: function () {
-    return (
-        <div>
-          <p>Good  morning!</p>
-        </div>
-    )
-
-  }
-})
-
-var Y = React.createClass({
-  render: function () {
-    return (
-        React.createElement('p', {}, " I'm a 'p' element created by React createElement")
-    )
-  }
-})
-
-
-class Profile extends React.Component {
+class MyComponent extends React.Component {
   render() {
-    let objects = this.props.objects.map(object => {
-      return (
-          <li>{object}</li>
-      )
-    })
-    return (
-      <div>
-        <h1>{this.props.name}</h1>
-        <p>{this.props.age}</p>
-        <ul>
-          {objects}
-        </ul>
-      </div>
+    return(
+        <div>
+          <p>This is a component</p>
+        </div>
     )
   }
 }
-
-
 class App extends React.Component {
+  componentWillMount (){
+    console.log('in componentWillMount')
+  }
+
+  componentDidMount () {
+    console.log("in componentDidMount")
+  }
   constructor(props) {
     super(props)
-    this.state = {
-      people: [
-        {name: "marcos" , age: '23', objects: ['pencil', 'eraser', 'pen']},
-        {name: "bob", age: '21', objects: ['pencil', 'paperSheet', 'clip']}
-      ]
-    }
-    this.addUser = this.addUser.bind(this)
-  }
-
-  addUser(e) {
-    var person = {name: "billy" , age: '22', objects: ['cup', 'mouse', 'keyboard']}
-    this.setState({
-      people : this.state.people.concat([person])
-    })
   }
 
   render() {
-    let people = this.state.people.map(person => {
-      return (
-          <Profile
-              name={person.name}
-              age={person.age}
-              objects={person.objects}
-          />
-      )
-
-    })
+    console.log('in render')
     return (
         <div>
-          {people}
-          <button onClick={this.addUser}>Add User</button>
+          <p>Hello world</p>
+          <MyComponent>
+              <AnotherComponent/>
+          </MyComponent>
+          <p>Hello </p>
         </div>
     )
   }
 }
-
-
-// var App = React.createClass({
-//   getInitialState: function () {
-//     return {
-//       people: [
-//         {name: "marcos" , age: '23', objects: ['pencil', 'eraser', 'pen']},
-//         {name: "bob", age: '21', objects: ['pencil', 'paperSheet', 'clip']}
-//       ]
-//     }
-//   },
-//   render: function () {
-//     var person1 = this.state.people[0].name
-//     return (
-//       <div>
-//         <h1>Hello world</h1>
-//         <p>Hi there</p>
-//         <X></X>
-//         <Y></Y>
-//         <p1>{person1}</p1>
-//       </div>
-//     )
-//   }
-// })
 
 ReactDOM.render(<App />, document.getElementById('app'));
