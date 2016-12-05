40198d11cb733151042bc09d9b80bb6f981d5403
Marcos Hosoya <marcos.hosoya@certsys.com.br>
2016-12-05 14:18:31 -0200

    altera profile.js

--- app/components/Profile.jsx
+++ app/components/Profile.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 
-export default class Profile extends React.Component {
-    render() {
-        let objects = this.props.objects.map(object => {
-            return (
-                <li>{object}</li>
-            )
-        })
-        return (
-            <div>
-                <h1>{this.props.name}</h1>
-                <p>{this.props.age}</p>
-                <ul>
-                    {objects}
-                </ul>
-            </div>
-        )
-    }
-}
+// export default class Profile extends React.Component {
+//     render() {
+//         let objects = this.props.objects.map(object => {
+//             return (
+//                 <li>{object}</li>
+//             )
+//         })
+//         return (
+//             <div>
+//                 <h1>{this.props.name}</h1>
+//                 <p>{this.props.age}</p>
+//                 <ul>
+//                     {objects}
+//                 </ul>
+//             </div>
+//         )
+//     }
+// }
+
+
+let Profile = props => (
+    <div>
+        <h1>{props.name}</h1>
+        <p>{props.age}</p>
+        <ul>
+            {props.objects.map( object => <li>{object}</li>)}
+        </ul>
+    </div>
+)
+
+export default Profile
