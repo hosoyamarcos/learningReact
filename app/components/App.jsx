7536dff66feda35a99293d383ddeedd56dc6054a
Marcos Hosoya <marcos.hosoya@certsys.com.br>
2016-12-06 10:44:57 -0200

    higher order component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,43 +1,65 @@
 import React from 'react'
 
-import Profile from './Profile.jsx'
-import AddProfile from './AddProfile.jsx'
+let HigherOrderComponent = (Component, starter) => class extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: starter
+        }
+    }
+
+    componentDidMount () {
+        setInterval(() => {
+            this.setState({
+                count : this.state.count +1
+            })
+        },500)
+    }
+
+    render() {
+        return (
+            <Component {...this.props} {...this.state} />
+        )
+    }
+}
+
+class Comp1 extends React.Component {
+    render () {
+        return (
+            <div>
+                <p>Comp</p>
+                {this.props.count}
+            </div>
+        )
+    }
+}
+class Comp2 extends React.Component {
+    render () {
+        return (
+            <div>
+                <p>Comp</p>
+                {this.props.count}
+            </div>
+        )
+    }
+}
+
+let WrappedComp1 = HigherOrderComponent(Comp1, 0)
+let WrappedComp2 = HigherOrderComponent(Comp2, 100)
 
 export default class App extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            people: [
-                {name: "marcos" , age: '23', objects: ['pencil', 'eraser', 'pen']},
-                {name: "bob", age: '21', objects: ['pencil', 'paperSheet', 'clip']}
-            ]
-        }
-        this.addUser = this.addUser.bind(this)
-    }
 
-    addUser(newProfile) {
-        console.log(newProfile)
-        this.setState({
-            people : this.state.people.concat([newProfile])
-        })
     }
 
     render() {
-        let people = this.state.people.map(person => {
-            return (
-                <Profile
-                    name={person.name}
-                    age={person.age}
-                    objects={person.objects}
-                />
-            )
-
-        })
         return (
             <div>
-                {people}
-                <AddProfile addUser={this.addUser}/>
+                <WrappedComp1/>
+                <WrappedComp2/>
             </div>
+
         )
     }
 }
