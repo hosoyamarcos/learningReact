8edbfdec92f17b4f5b680d03879311fa956987b2
Marcos Hosoya <marcos.hosoya@certsys.com.br>
2016-12-06 11:58:53 -0200

    add PropTypes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,63 +1,36 @@
 import React from 'react'
 
-let HigherOrderComponent = (Component, starter) => class extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            count: starter
-        }
-    }
-
-    componentDidMount () {
-        setInterval(() => {
-            this.setState({
-                count : this.state.count +1
-            })
-        },500)
-    }
-
-    render() {
-        return (
-            <Component {...this.props} {...this.state} />
-        )
-    }
-}
-
-class Comp1 extends React.Component {
+class MyComponent extends React.Component {
     render () {
         return (
             <div>
-                <p>Comp</p>
-                {this.props.count}
-            </div>
-        )
-    }
-}
-class Comp2 extends React.Component {
-    render () {
-        return (
-            <div>
-                <p>Comp</p>
-                {this.props.count}
+                <p>Name: {this.props.name}</p>
+                <p>Age: {this.props.age} </p>
             </div>
         )
     }
 }
 
-let WrappedComp1 = HigherOrderComponent(Comp1, 0)
-let WrappedComp2 = HigherOrderComponent(Comp2, 100)
+MyComponent.propTypes = {
+    name: React.PropTypes.string.isRequired,
+    age: React.PropTypes.number.isRequired
+}
+
+MyComponent.defaultProps = {
+    name : 'Myname',
+    age: 0
+}
+
 
 export default class App extends React.Component {
     constructor(props) {
         super(props)
-
     }
 
     render() {
         return (
             <div>
-                <WrappedComp1/>
-                <WrappedComp2/>
+                <MyComponent name={'billy'} age={4}/>
             </div>
 
         )
